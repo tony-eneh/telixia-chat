2a705cfcd6cfc32649f3753d108acad1a2248415
Tony Eneh <anthony.u.eneh@gmail.com>
2022-07-16 14:30:10 +0100

    Feat: introduce routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import Chat from './pages/Chat/Chat';
+import Chats from './pages/Chats/Chats';
+import Signup from './pages/Signup/Signup';
 
 const App = () => {
   return (
-    <div>
-      some text
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chats" element={<Chats />} />
+        <Route path="/chats/:chatId" element={<Chat />} />
+        <Route path="*" element={<Navigate to="/chats" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
